bff7a12401dd655e627896ad8cfe0163868c9ec2
Tyson Shepherd <tysonthedev@gmail.com>
2021-01-12 18:37:23 -0700

    seperated logic from requests, made planets endpoint more efficient
    Used nested promise.all for planets endpoint for a much faster endpoint.

--- starwarsapi.js
+++ starwarsapi.js
@@ -3,11 +3,7 @@ const app = express();
 const swapiWrapper = require('./swapiWrapper');
 const hostname = '127.0.0.1';
 const port = 5000;
-//swapi.dev
-//swapi-deno
-//const baseUrl = 'https://swapi-deno.azurewebsites.net/api/';
 
-//swapi.dev
 const getAllPeople = async (sortBy) => {
 	const parseNumber = (whichNumber) => {
 		whichNumber = whichNumber.replace(',', '');
@@ -16,20 +12,28 @@ const getAllPeople = async (sortBy) => {
 		} else return parseInt(whichNumber);
 	};
 	const sortByName = (whatToSort) => {
-		whatToSort.results = whatToSort.results.sort(function (a, b) {
+		whatToSort = whatToSort.sort(function (a, b) {
 			return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
 		});
 		return whatToSort;
 	};
 	const sortByHeight = (whatToSort) => {
-		whatToSort.results = whatToSort.results.sort(function (a, b) {
-			return parseNumber(a.height) < parseNumber(b.height) ? -1 : parseNumber(a.height) > parseNumber(b.height) ? 1 : 0;
+		whatToSort = whatToSort.sort(function (a, b) {
+			return parseNumber(a.height) < parseNumber(b.height)
+				? -1
+				: parseNumber(a.height) > parseNumber(b.height)
+				? 1
+				: 0;
 		});
 		return whatToSort;
 	};
 	const sortByMass = (whatToSort) => {
-		whatToSort.results = whatToSort.results.sort(function (a, b) {
-			return parseNumber(a.mass) < parseNumber(b.mass) ? -1 : parseNumber(a.mass) > parseNumber(b.mass) ? 1 : 0;
+		whatToSort = whatToSort.sort(function (a, b) {
+			return parseNumber(a.mass) < parseNumber(b.mass)
+				? -1
+				: parseNumber(a.mass) > parseNumber(b.mass)
+				? 1
+				: 0;
 		});
 		return whatToSort;
 	};
@@ -44,115 +48,14 @@ const getAllPeople = async (sortBy) => {
 		}
 	}
 
-	var allPeople = await swapiWrapper.fetchAllPeople();
-	return allPeople;
+	let allPeople = await swapiWrapper.fetchAllPeople();
 	if (sortBy != undefined) return sortTypes[sortBy](allPeople);
 	else return allPeople;
 };
-//swapi-deno
-/*
-const getAllPeople = async (sortBy) => {
-	const parseNumber = (whichNumber) => {
-		whichNumber = whichNumber.replace(',', '');
-		if (isNaN(whichNumber)) {
-			return 0;
-		} else return parseInt(whichNumber);
-	};
-	const sortByName = (whatToSort) => {
-		whatToSort.results = whatToSort.results.sort(function (a, b) {
-			return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
-		});
-		return whatToSort;
-	};
-	const sortByHeight = (whatToSort) => {
-		whatToSort.results = whatToSort.results.sort(function (a, b) {
-			return parseNumber(a.height) < parseNumber(b.height) ? -1 : parseNumber(a.height) > parseNumber(b.height) ? 1 : 0;
-		});
-		return whatToSort;
-	};
-	const sortByMass = (whatToSort) => {
-		whatToSort.results = whatToSort.results.sort(function (a, b) {
-			return parseNumber(a.mass) < parseNumber(b.mass) ? -1 : parseNumber(a.mass) > parseNumber(b.mass) ? 1 : 0;
-		});
-		return whatToSort;
-	};
-	const sortTypes = {
-		name: sortByName,
-		height: sortByHeight,
-		mass: sortByMass,
-	};
-	if (sortBy != undefined) {
-		if (sortTypes[sortBy] == undefined) {
-			return { error: `Invalid sortBy. Valid values are: ${Object.keys(sortTypes)}` };
-		}
-	}
-
-	var allPeople = { results: [] };
-	try {
-		var returnData = await (await axios.get(`${baseUrl}people/`)).data;
-		nextPage = returnData['next'];
-		allPeople.results.push(...returnData);
-	} catch (error) {
-		return { error: `All People Error: ${error}` };
-	}
-	if (sortBy != undefined) return sortTypes[sortBy](allPeople);
-	else return allPeople;
-};
-*/
 //swapi.dev
 const getAllPlanets = async () => {
-	var allPlanets = { results: [] };
-	var nextPage = `${baseUrl}planets/`;
-	while (nextPage != null || nextPage != undefined) {
-		try {
-			const apiCall = await axios.get(nextPage);
-			var returnData = apiCall.data;
-			nextPage = returnData['next'];
-			for await (let planet of returnData.results) {
-				if (planet.residents != undefined) {
-					const planetResidentsPromise = await axios.all(
-						planet.residents.map((residentUrl) => {
-							return axios.get(residentUrl);
-						})
-					);
-					planet.residents = planetResidentsPromise.map((promiseObject) => {
-						return promiseObject.data.name;
-					});
-				}
-			}
-			allPlanets.results.push(...returnData['results']);
-		} catch (error) {
-			return { error: `All Planets Error: ${error}` };
-		}
-	}
-	return allPlanets;
+	return await swapiWrapper.fetchAllPlanets();
 };
-//swapi-deno
-/*
-const getAllPlanets = async () => {
-	var allPlanets = { results: [] };
-	try {
-		const apiCall = await axios.get(`${baseUrl}planets/`);
-		var returnData = apiCall.data;
-		for await (let planet of returnData) {
-			if (planet.residents != undefined) {
-				const planetResidentsPromise = await axios.all(
-					planet.residents.map((residentId) => {
-						return axios.get(`${baseUrl}people/${residentId}`);
-					})
-				);
-				planet.residents = planetResidentsPromise.map((promiseObject) => {
-					return promiseObject.data.name;
-				});
-			}
-		}
-		allPlanets.results.push(...returnData);
-	} catch (error) {
-		return { error: `All Planets Error: ${error}` };
-	}
-	return allPlanets;
-};
-*/
 app.get('/people', async (req, res) => {
 	const allPeople = await getAllPeople(req.query.sortBy);
 	res.json(allPeople);
@@ -164,9 +67,7 @@ app.get('/planets', async (req, res) => {
 });
 
 const server = app.listen(port, function () {
-	//only for swapi.dev due to ssl cert being expired
-	//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-	var host = server.address().address;
-	var port = server.address().port;
+	let host = server.address().address;
+	let port = server.address().port;
 	console.log('Node JS server started!');
 });
--- swapiWrapper.js
+++ swapiWrapper.js
@@ -1,5 +1,5 @@
-const baseUrl = 'https://swapi.dev/api/';
 const axios = require('axios');
+const baseUrl = 'https://swapi.dev/api/';
 
 const fetchAllPeople = async () => {
 	let firstPage = await axios.get(`${baseUrl}people/`);
@@ -8,16 +8,47 @@ const fetchAllPeople = async () => {
 	for (let i = 2; i <= pageCount; i++) {
 		pageLinks.push(`${baseUrl}people/?page=${i}`);
 	}
-	const allPages = await Promise.all(
+	const lastPages = await Promise.all(
 		pageLinks.map((link) => {
 			return axios.get(link);
 		})
 	);
-	const result = allPages.reduce((acc, current) => {
+	const result = lastPages.reduce((acc, current) => {
 		return acc.concat(current.data.results);
 	}, firstPage.data.results);
 	return result;
 };
-const fetchAllPlanets = async () => {};
+const fetchAllPlanets = async () => {
+	let firstPage = await axios.get(`${baseUrl}planets/`);
+	let pageCount = Math.ceil(firstPage.data.count / firstPage.data.results.length);
+	let pageLinks = [];
+	for (let i = 2; i <= pageCount; i++) {
+		pageLinks.push(`${baseUrl}planets/?page=${i}`);
+	}
+	const lastPages = await Promise.all(
+		pageLinks.map((link) => {
+			return axios.get(link);
+		})
+	);
+	const allPlanets = lastPages.reduce((acc, current) => {
+		return acc.concat(current.data.results);
+	}, firstPage.data.results);
+
+	const result = Promise.all(
+		allPlanets.map(async (planet) => {
+			const residentPages = await Promise.all(
+				planet.residents.map(async (residentLink) => {
+					return axios.get(residentLink);
+				})
+			);
+			const residentNames = residentPages.map((residentPage) => {
+				return residentPage.data.name;
+			});
+			planet.residents = residentNames;
+			return planet;
+		})
+	);
+	return result;
+};
 
 module.exports = { fetchAllPeople, fetchAllPlanets };
